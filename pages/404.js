3d1b023f7832d20c7ee9e0612e04159ec35c1085
Fabian Fernandez <fdzfabian@gmail.com>
2021-11-23 19:45:38 -0600

    Initial commit

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export default function Cus404() {
+  return (
+    <Layout>
+      <Head>
+        <title>404 - Not found</title>
+      </Head>
+      <h1>404 - Page Not Found</h1>
+    </Layout>
+  )
+}
